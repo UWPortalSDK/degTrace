5d67c2cb7434690dca328f360f86131141b966fc
Kevin Miao <33260828+MrMiaoMiao@users.noreply.github.com>
2017-11-05 00:29:15 -0400

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -1,12 +1,6 @@
 angular.module('portalApp')
     .controller('degTraceCtrl', ['$scope', function($scope, $http) {
 
-        $scope.first = 10;
-
-        var myCourses = new Object(); //store own courses
-        myCourses[0] = "CS/137";
-        var courseData = new Object();
-
         // mock data
         $scope.items = [{
             title: "hi",
@@ -24,8 +18,11 @@ angular.module('portalApp')
             $scope.portalHelpers.showView('degTraceDetails.html', 2);
         }
 
+        
         //$scope.fetch()
-
+		var myCourses = new Object(); //store own courses
+        myCourses[0] = "CS/137";
+        var courseData = new Object();
         function fetch() {
             $http.get("https://api.uwaterloo.ca/v2/courses/" + myCourses[0] + "/prerequisites.json/?key=152d64003d18c172c4d33ac35f42c28e")
                 .then(function(response) {
@@ -34,35 +31,3 @@ angular.module('portalApp')
         }
 
     }]);
-
-/*
-
-
-$http.post("/Develop/GetProxy", {
-    url: "https://api.uwaterloo.ca/v2/key=152d64003d18c172c4d33ac35f42c28e"
-}).success(function(response) {
-    for (var k in myCourses) {
-        courseData[k] = GET / courses / myCourses[k] / prerequisites.json
-        $.getJSON(courseData[k], {
-            subject: ,
-            catalog_number: ,
-            title: ,
-            prerequisites: ,
-            prerequisites_parsed:
-        })
-    }
-});*/
-
-
-
-/* G A R B A G E (probably)
-        $http({
-            method: 'GET',
-            url: 'https://api.uwaterloo.ca/v2/courses/CS/137/prerequisites.json'
-        }).then(function successCallback(response) {
-            $scope.courseData[0] = response;
-            window.alert(courseData[0]);
-        }, function errorCallback(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });*/
